8be6f22a963cba2ecf71893736895674f470e55c
Jayant Dash <jayant_dash@comcast.com>
2019-08-21 21:22:45 -0400

    initial commit

---
+++ SFF/Numbers/Double+SFF.swift
@@ -0,0 +1,9 @@
+//
+//  Double+SFF.swift
+//  SFF
+//
+//  Created by Dash, Jayant on 7/24/19.
+//  Copyright © 2019 Jayant Dash. All rights reserved.
+//
+
+import Foundation
---
+++ SFF/SFF.h
@@ -0,0 +1,19 @@
+//
+//  SFF.h
+//  SFF
+//
+//  Created by Dash, Jayant on 7/24/19.
+//  Copyright © 2019 Jayant Dash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFF.
+FOUNDATION_EXPORT double SFFVersionNumber;
+
+//! Project version string for SFF.
+FOUNDATION_EXPORT const unsigned char SFFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFF/PublicHeader.h>
+
+
